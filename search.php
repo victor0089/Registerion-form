724ffff5109c7084b440d5059f4fff06f0a03053
Engineer V <victor.mecdoors@gmail.com>
2023-05-05 22:18:44 +0300

    adding search page

---
+++ search.php
@@ -0,0 +1,21 @@
+
+<html>
+<head>
+
+</head>
+
+
+<body>
+<main>
+	<form action="searchdb.php" method="post">
+		<input
+			type="text"
+			placeholder="Enter your search term"
+			name="search"
+			required>
+		<button type="submit" name="submit">Search</button>
+	</form>
+</main>
+
+</body>
+</html>
